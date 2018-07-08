cf22b099092e6770095981ebb70eb36c2fe694cc
winkhanh <giakhanhnk2000@gmail.com>
2018-07-08 19:19:05 +0700

    done basic snake

--- Assets/Scripts/Sunlight.cs
+++ Assets/Scripts/Sunlight.cs
@@ -4,37 +4,22 @@ using UnityEngine;
 
 public class Sunlight : MonoBehaviour {
     public Vector3 center;
-    public Vector3 beginPosition;
-    public Vector3 endPosition;
-    public float speed = 10f;
-    public float dayend = 0;
-    public bool ifday = true;
-    public float retime;
-
+    
+    public float range;
+    public float speed;
     // light is set using unity-editor
     public GameObject light;
 	// Update is called once per frame
+
 	void Update () {
-        retime = Time.time;
-        Vector3 temp;
-        temp.x = beginPosition.x - (beginPosition.x - endPosition.x) * (Time.time - dayend) / speed;
-        temp.y = beginPosition.y - (beginPosition.y - endPosition.y) * (Time.time - dayend) / speed;
-        temp.z = beginPosition.z - (beginPosition.z - endPosition.z) * (Time.time - dayend) / speed;
-        if (ifday) light.transform.position = temp;
-        if (Time.time - dayend > speed)
-            if (!ifday)
-        {
-            dayend = Time.time;
-            light.transform.position = beginPosition;
-            ifday = true;
-            light.SetActive(true);
-        }
-        else
-        {
-            dayend = Time.time;
-            ifday = false;
-            light.SetActive(false);
-        }
-        
+        move(); 
 	}
+    void move()
+    {
+        Vector3 temp;
+        temp.x = Mathf.Sin(Time.time/speed) * range;
+        temp.z = center.z;
+        temp.y = Mathf.Cos(Time.time/speed) * range;
+        light.transform.position = temp;
+    }
 }
